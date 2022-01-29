a863923ac31bfb1540e69dd10eaf8795966ceae8
tatarmagda <tatar.magdalena@wp.pl>
2022-01-29 15:07:43 +0100

    Zmiany1

--- lib/Screens/firstscreen.dart
+++ lib/Screens/firstscreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class FirstScreen extends StatefulWidget {
@@ -8,18 +10,48 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  var hight = Random().nextInt(500);
+  var widht = Random().nextInt(500);
+  List colorList = [
+    Colors.purple[300],
+    Colors.purple[200],
+    Colors.pink[100],
+    Colors.red[200],
+    Colors.red[300],
+    Colors.red[400],
+    Colors.red[500],
+  ];
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Container(
-          child: Center(child: Text("Cześś, jestem wesoły Romek")),
-          color: Colors.lightBlue[300],
-          width: 200.0,
-          height: 200.0,
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Container(
+              child: Center(child: Text("Cześś, jestem wesoły Romek")),
+              color: colorList[index],
+              width: widht.toDouble(),
+              height: hight.toDouble(),
+            ),
+          ),
         ),
-        ButtonBar(),
+        MaterialButton(
+          onPressed: () {
+            setState(() {
+              hight = Random().nextInt(500);
+              widht = Random().nextInt(500);
+              index = Random().nextInt(colorList.length - 1);
+            });
+          },
+          child: Text("Click me"),
+          color: Colors.blue[900],
+          shape: StadiumBorder(),
+        )
       ],
     ));
   }
